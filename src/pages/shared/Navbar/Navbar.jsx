821d832b215647e697edcb851c7059827dedc788
ArifMiah07 <arifmiah.me322@gmail.com>
2024-10-15 23:36:19 +0600

    make nav as assingment requierment--- [closely done]

--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -1,9 +1,59 @@
 import { NavLink } from 'react-router-dom';
 import eco_wonderland from '../../../../public/images/eco-wonderland-cropted.png'
 import '../../../index.css'
+import CustomDropdown from '../../../components/CustomDropdown';
 
 
 const Navbar = () => {
+
+    const navLinks = (
+        <>
+          <li>
+            <NavLink to="/" className={({ isActive }) =>
+              isActive
+                ? 'text-[#fff] rounded-0 work-sans font-semibold text-[18px] px-3 py-2'
+                : 'text-[18px]'
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <CustomDropdown />
+          <li>
+            <NavLink to="/add-tourist-spot" className={({ isActive }) =>
+              isActive
+                ? 'text-[#fff] rounded-0 work-sans font-semibold text-[18px] px-3 py-2'
+                : 'text-[18px]'
+              }
+            >
+               Add Tourists Spot
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/my-list" className={({ isActive }) =>
+              isActive
+                ? 'text-[#fff] rounded-0 work-sans font-semibold text-[18px] px-3 py-2'
+                : 'text-[18px]'
+              }
+            >
+              My List
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/register" className={({ isActive }) =>
+              isActive
+                ? 'text-[#fff] rounded-0 work-sans font-semibold text-[18px] px-3 py-2'
+                : 'text-[18px]'
+              }
+            >
+              Register
+            </NavLink>
+          </li>
+        </>
+      );
+      
+      
+
     return (
         <div className="roboto navbar flex justify-between px-12 lg:flex lg:flex-col ">
             <div className="navbar-start lg:flex lg:flex-col lg:items-center ">
@@ -18,25 +68,13 @@ const Navbar = () => {
                     <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                 </div>
                 <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                    <li><NavLink to="/" className={({isActive})=> isActive ? 'text-[#fff] rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'}>Home</NavLink></li>
-                    <li> <NavLink to="/asia"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'}  > Asia </NavLink> </li> 
-                    <li> <NavLink to="/america"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'}  > America </NavLink> </li> 
-                    <li> <NavLink to="/europe"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'  }> Europe </NavLink> </li> 
-                    <li> <NavLink to="/australia"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'}  > Australia </NavLink> </li> 
-                    <li> <NavLink to="/africa"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'  }> Africa </NavLink> </li> 
-                    <li> <NavLink to="/about"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'  }> About</NavLink> </li> 
+                        {navLinks}
                     </ul>
                 </div>
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">
-                <li><NavLink to="/" className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'}>Home</NavLink></li>
-                    <li> <NavLink to="/asia"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'}  > Asia </NavLink> </li> 
-                    <li> <NavLink to="/america"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'}  > America </NavLink> </li> 
-                    <li> <NavLink to="/europe"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'  }> Europe </NavLink> </li> 
-                    <li> <NavLink to="/australia"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'}  > Australia </NavLink> </li> 
-                    <li> <NavLink to="/africa"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'  }> Africa </NavLink> </li> 
-                    <li> <NavLink to="/about"  className={({isActive})=> isActive ? 'text-[#fff]  rounded-0 work-sans font-semibold text-[18px] px-3 py-2' : ' text-[18px]'  }> About </NavLink> </li> 
+                    {navLinks}
                 </ul>
             </div>
             
